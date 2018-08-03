4769d2ba367099f6ec6612800f483188416cb674
Jorge Diaz <jorge7soccer@gmail.com>
2018-08-03 13:38:54 -0400

    ENH: Updated __init__.

--- databroker_extensions/__init__.py
+++ databroker_extensions/__init__.py
@@ -1,11 +1,11 @@
 from databroker import Broker
+from databroker.assets.handlers import AreaDetectorTiffHandler
+from eiger_io.fs_handler import EigerHandler
 import pandas as pd
 import os
 import datetime
 import time
 from time import mktime
-from eiger_io.fs_handler import EigerHandler
-from databroker.assets.handlers import AreaDetectorTiffHandler
 from pymongo.errors import CursorNotFound
 from collections import defaultdict
 from .file_usage import file_sizes, get_file_size
